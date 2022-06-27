0baef7801238bbc36dbd61778b5ef2f4676e1bb8
npanj <nat.panjar@gmail.com>
2022-06-27 17:26:14 +0700

    main: complete softmax

--- project/mnist/part1/main.py
+++ project/mnist/part1/main.py
@@ -124,6 +124,11 @@ print('softmax test_error=', run_softmax_on_MNIST(temp_parameter=1))
 # TODO: Find the error rate for temp_parameter = [.5, 1.0, 2.0]
 #      Remember to return the tempParameter to 1, and re-run run_softmax_on_MNIST
 
+temp_parameters = [.5 , 1, 2]
+
+for temp in temp_parameters:
+    print(f"softmax test_error with = temperature at {temp}", run_softmax_on_MNIST(temp_parameter=temp))
+    
 #######################################################################
 # 6. Changing Labels
 #######################################################################
